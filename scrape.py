b9da2117549285a51d0fb90d5cc14beb2a5fbb92
Mohamed-Ayaan358 <ayaan35813@gmail.com>
2021-10-30 00:01:02 +0530

    Added scraping procedure using flask

--- scrape.py
+++ scrape.py
@@ -1,17 +1,20 @@
 from bs4 import BeautifulSoup
 from requests import get
-# import pandas as pd
+import pandas as pd
 from tqdm import tqdm
 from dateutil import parser
 import string
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
 
 # Checking for the total no. of pages
 
-url = 'https://timesofindia.indiatimes.com/topic/Sanitizer/news/'
+url = 'https://timesofindia.indiatimes.com/topic/coronavirus-india'
 soup = BeautifulSoup(get(url).text, 'lxml')
 
 ##Because the website displays ages only till 20
-max_urls = [url + str(i) for i in range(1, 5)]
+max_urls = [url + str(i) for i in range(1, 21)]
 
 # Creating empty lists to save all the features
 headlines, dates, news, urls = [], [], [], []
@@ -26,7 +29,7 @@ for index in max_urls:
         # Extracts the Headlines
         try:
             headline = [soup.select('span.title')[i].text.strip() for i in range(len(soup.select('span.title')))]
-            print(headline)
+            #print(headline)
             headlines.extend(headline)
         except:
             headlines.extend(None)
@@ -53,6 +56,7 @@ for index in max_urls:
 print("[INFO] Links Extracted.")
 
 print("The total no. of pages is=", len(urls))
+# print(set(dates))
 print("No. articles=", len(dates))
 print("Last article goes back till: ", min(dates))
 
@@ -77,3 +81,31 @@ for index in tqdm(urls):
         news.append(None)
 
 print("[INFO] Articles Extracted.")
+
+df = pd.DataFrame({'Headlines': headlines,
+                   'Article': news,
+                   'Published_Dates': dates,
+                   'Source_URLs': urls
+                   })
+print(df.head(5))
+
+headlines=list(df.head(10)['Headlines'])
+sources=list(df.head(10)['Source_URLs'])
+dates=list(df.head(10)['Published_Dates'])
+print(headlines)
+
+@app.route('/news.html')
+def news():
+    return render_template('news.html', headlines=headlines, sources=sources, dates=dates)
+
+@app.route('/')
+@app.route('/index.html')
+def index():
+    return render_template('index.html')
+
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
